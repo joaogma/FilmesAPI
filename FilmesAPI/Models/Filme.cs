081488af518ea6c54842446eb376a88678eca5ef
joaogma <mx.joaoo@gmail.com>
2021-10-29 18:10:51 -0300

    Add project files.

---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Required(ErrorMessage = "O campo Título é obrigatório.")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo Diretor é obriatório.")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O gênero passou de 30 caracteres")]
+        public string Genero { get; set; }
+        [Range(1,600, ErrorMessage = "A duração deve ser entre 0 e 600 minutos.")]
+        public int Duracao { get; set; }
+    }
+}
